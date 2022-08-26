f9aa103e77d4847c1519819891d5074454a41c04
calebflores <calebflores1388@gmail.com>
2022-08-26 10:41:14 -0700

    name migration

---
+++ backend/db/migrations/20220826172955-lastname.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn('Users', 'lastName', {
+      type: Sequelize.STRING
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeColumn('Users', 'lastName');
+  }
+};
