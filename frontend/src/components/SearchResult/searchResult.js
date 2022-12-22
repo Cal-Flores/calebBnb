1548994705fcbc7904e4987da43bfa4ce1982563
calebflores <calebflores1388@gmail.com>
2022-12-21 19:54:23 -0700

    no results

--- frontend/src/components/SearchResult/searchResult.js
+++ frontend/src/components/SearchResult/searchResult.js
@@ -9,27 +9,40 @@ function SearchedResult() {
     const searchArr = Object.values(searchObj)
     console.log('this is searchobj', searchArr)
 
+    let res;
+    if (searchArr.length === 0) {
+        res = false
+    }
     return (
-        <div>
-            <div>Hello to you stranger</div>
-            <div>
+        <div className='srcont'>
+            <div className='srheader'>{searchArr?.length} Spots in the area:</div>
+            <div className='srcont'>
+                {!res &&
+                    <div className='nsrcont'>
+                        <div className='nsrlogo'><i class="fa-brands fa-airbnb"></i></div>
+                        <div className='nsrtext'>Be the first to host a spot in the area!</div>
+                    </div>
+                }
                 {searchArr?.map(spot => (
-                    <div className='searchcont'>
-                        <div className='searchwrap'>
+                    <div >
+                        <div>
                             <Link className="spotcard" key={spot.id} to={`/spots/${spot.id}`}>
                                 <div>
                                     <img className="spotimg" src={spot?.image} onError={(e) => { e.target.src = 'https://i0.wp.com/www.careandshare-ut.org/wp-content/uploads/2020/09/image-coming-soon.jpg?fit=1200%2C1200&ssl=1' }}></img>
                                 </div>
-                                <div className="infowrapper">
-                                    <div className="spotloc"> {spot.city}, {spot.state}</div>
-                                    {/* <div className="spotrate"> &#9733; {spot.avgRating}</div> */}
+                                <div className='srdetail'>
+                                    <div className='srdet'>
+                                        <div> {spot.city}, {spot.state}</div>
+                                        {/* <div className="spotrate"> &#9733; {spot.avgRating}</div> */}
+                                    </div>
+                                    <div className='srdet'>
+                                        {spot.name}
+                                    </div>
+                                    <div className='srdet'>
+                                        <span>${spot.price} </span><span className="night">night</span>
+                                    </div>
                                 </div>
-                                <div className="spotname">
-                                    {spot.name}
-                                </div>
-                                <div className="price">
-                                    <span className="spotprice">${spot.price} </span><span className="night">night</span>
-                                </div>
+
                             </Link>
                         </div>
                     </div>
