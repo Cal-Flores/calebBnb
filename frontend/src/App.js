084bf453c74809e6adc727a6ce2b03b68540f44f
calebflores <calebflores1388@gmail.com>
2022-12-23 10:00:51 -0700

    style forms

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,14 +6,8 @@ import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
 import AllSpots from "./components/SplashPage";
 import SpotDetail from "./components/spotDetail";
-import EditSpotForm from "./components/EditASpot";
-import DeleteASpot from "./components/DeleteASpot";
-import ReviewSpot from "./components/ReviewsBySpot";
-import CreateReiew from "./components/createReview";
 import MyProfile from "./components/Your-Content";
 import './index.css'
-import Editreview from "./components/EditAReview";
-import EditBooking from "./components/EditA Booking";
 import Footer from "./components/Footer/footer";
 import SearchedResult from "./components/SearchResult/searchResult";
 
@@ -42,26 +36,8 @@ function App() {
           <Route exact path="/spots/:spotId">
             <SpotDetail />
           </Route>
-          <Route exact path='/spots/edit/:spotId'>
-            <EditSpotForm />
-          </Route>
-          <Route exact path='/spots/delete/:spotId'>
-            <DeleteASpot />
-          </Route>
-          <Route exact path='/spots/reviews/:spotId'>
-            <ReviewSpot />
-          </Route>
-          <Route exact path='/spots/:spotId/create-review'>
-            <CreateReiew />
-          </Route>
           <Route exact path='/my-profile'>
             < MyProfile />
-          </Route>
-          <Route exact path='/reviews/edit/:reviewId'>
-            <Editreview />
-          </Route>
-          <Route exact path='/booking/edit/:bookingId'>
-            <EditBooking />
           </Route>
         </Switch>
       )}
