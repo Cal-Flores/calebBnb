4f464c79a17bbf7a6435d975f64b852e7e89c412
calebflores <calebflores1388@gmail.com>
2023-03-31 17:09:28 -0500

    likes model

---
+++ backend/db/seeders/20230331220318-likes.js
@@ -0,0 +1,23 @@
+'use strict';
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    options.tableName = 'Reviews';
+    await queryInterface.bulkInsert(
+      options,
+      [],
+      {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
